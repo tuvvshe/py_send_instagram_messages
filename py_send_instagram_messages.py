74317074be095061424583112235f51a68eaa426
Tuvshuu <87944616+tuvvshe@users.noreply.github.com>
2023-07-22 19:41:30 +0200

    Add files via upload

---
+++ py_send_instagram_messages.py
@@ -0,0 +1,8 @@
+from instabot import Bot
+bot = Bot()
+
+
+bot.login(username = "UR USERNAME",
+          password = "UR Password")
+bot.send_message("WAZAAAP!!!", 
+                 ["Receiver's Username"])
